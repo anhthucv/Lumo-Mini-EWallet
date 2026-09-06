a254bb5d04129ba817e5e9086c31e8f0cae58d3b
cari <anhthuchevu@gmail.com>
2026-09-06 13:46:29 +0700

    fix: remove incompatible security auto-configuration exclusion

--- payment-ledger-service/src/main/java/com/chethu/paymentledgerservice/PaymentLedgerServiceApplication.java
+++ payment-ledger-service/src/main/java/com/chethu/paymentledgerservice/PaymentLedgerServiceApplication.java
@@ -2,13 +2,11 @@ package com.chethu.paymentledgerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+@SpringBootApplication
 public class PaymentLedgerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentLedgerServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentLedgerServiceApplication.class, args);
+    }
 }
